0ea0061627e5717c4302df6bc86555dbdbc9cbde
Swastik Bhattacharjee <159615363+SwastikBhattacharjee@users.noreply.github.com>
2024-12-12 16:49:47 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-
 from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 import json
 from werkzeug.exceptions import HTTPException
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Initialize global soup object to prevent multiple requests
 URL = "http://donboscoberhampore.in/"
